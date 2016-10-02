0282e36a813c19108a31c52d0182e585a9f196d9
Adam Smith <adam@imaginate.life>
2016-10-01 22:35:43 -0700

    Added lib func: _sgl_which

---
+++ src/lib/_sgl_which.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# @dest /lib/superglue/_sgl_which
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2016 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+#
+# @use _sgl_source which
+# @return
+#   0  PASS
+################################################################################
+
+############################################################
+# @func _sgl_which
+# @use _sgl_which CMD
+# @val CMD  Must be an executable.
+# @return
+#   0  PASS
+############################################################
+_sgl_which()
+{
+  local cmd="$1"
+
+  if [[ -x "/bin/$1" ]]; then
+    cmd="/bin/$1"
+  elif [[ -x "/usr/bin/$1" ]]; then
+    cmd="/usr/bin/$1"
+  elif [[ -x "/usr/local/bin/$1" ]]; then
+    cmd="/usr/local/bin/$1"
+  fi
+  printf '%s' "${cmd}"
+}
