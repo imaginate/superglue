f5f30bb05f77534c80e053e075cea2a04fb1df66
Adam Smith <adam@imaginate.life>
2016-10-01 19:03:40 -0700

    Added lib func: _sgl_source

---
+++ src/lib/_sgl_source.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# @dest /lib/superglue/_sgl_source
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2016 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+#
+# @use . /lib/superglue/_sgl_source
+# @return
+#   0  PASS
+################################################################################
+
+############################################################
+# @func _sgl_source
+# @use _sgl_source ...FUNC
+# @val FUNC  Must be a private `superglue' command.
+# @return
+#   0  PASS
+############################################################
+_sgl_source()
+{
+  local path
+
+  while [[ $# -gt 0 ]]; do
+
+    path="${SGL_LIB}/_sgl_$1"
+
+    if [[ ! -f "${path}" ]]; then
+      printf "%s\n" "DEP ERROR missing core func - reinstall \`superglue'" 1>&2
+      exit 5
+    fi
+
+    . "${path}"
+    shift
+  done
+}
