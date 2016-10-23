b0dbee070c4f37291cb08cfad17913f8e3857d76
Adam Smith <adam@imaginate.life>
2016-10-22 18:45:25 -0700

    Renamed _sgl_unset_each_func to _sgl_unset_funcs

--- src/lib/_sgl_unset_funcs.sh
+++ src/lib/_sgl_unset_funcs.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #
-# @dest /lib/superglue/_sgl_unset_each_func
+# @dest /lib/superglue/_sgl_unset_funcs
 #
 # @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
 # @copyright 2016 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
 #
-# @use _sgl_source unset_each_func
+# @use _sgl_source unset_funcs
 # @return
 #   0  PASS
 ################################################################################
 
 ############################################################
-# @func _sgl_unset_each_func
-# @use _sgl_unset_each_func ...BUILTIN
+# @func _sgl_unset_funcs
+# @use _sgl_unset_funcs ...BUILTIN
 # @val BUILTIN  Must be a builtin command.
 # @return
 #   0  PASS
 ############################################################
-_sgl_unset_each_func()
+_sgl_unset_funcs()
 {
   while [[ $# -gt 0 ]]; do
     _sgl_unset_func $1
     shift
   done
 }
-readonly -f _sgl_unset_each_func
+readonly -f _sgl_unset_funcs
