01a4f5340de63a7256e623b87be26c257dff860e
Adam Smith <adam@imaginate.life>
2017-02-13 02:41:48 -0800

    Added helper: _sgl_is_name

---
+++ src/incl/_sgl_is_name.sh
@@ -0,0 +1,30 @@
+#!/bin/bash --posix
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+################################################################################
+
+############################################################
+# @func _sgl_is_name
+# @use _sgl_is_name PATH
+# @val PATH  Should be a valid file system path.
+# @return
+#   0  PASS
+#   1  FAIL
+############################################################
+_sgl_is_name()
+{
+  local name
+
+  name="${1%/}"
+  name="${name##*/}"
+
+  if [[ -n "${name}" ]]      && \
+     [[ "${name}" != '*' ]]  && \
+     [[ "${name}" != '.*' ]]
+  then
+    return 0
+  fi
+  return 1
+}
+readonly -f _sgl_is_name
