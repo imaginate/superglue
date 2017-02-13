02cd974a1ec8fcae80be790fd97c1620c2b07a04
Adam Smith <adam@imaginate.life>
2017-02-13 01:31:24 -0800

    Rebuilt helper: _sgl_fail

--- src/incl/_sgl_fail.sh
+++ src/incl/_sgl_fail.sh
@@ -7,25 +7,26 @@
 ############################################################
 # @func _sgl_fail
 # @use _sgl_fail ERR MSG
-# @val MSG  Can be any string.
 # @val ERR  Must be an error from the below options or any valid integer in the
 #           range of `1' to `126'.
-#   `MISC'  An unknown error.
-#   `OPT'   An invalid option.
-#   `VAL'   An invalid or missing value.
-#   `AUTH'  A permissions error.
-#   `DPND'  A dependency error.
-#   `CHLD'  A child process exited unsuccessfully.
-#   `SGL'   A `superglue' script error.
+#   `ERR|MISC'  An unknown error.
+#   `OPT'       An invalid option.
+#   `VAL'       An invalid or missing value.
+#   `AUTH'      A permissions error.
+#   `DPND'      A dependency error.
+#   `CHLD'      A child process exited unsuccessfully.
+#   `SGL'       A `superglue' script error.
+# @val MSG  Can be any string.
 # @return
 #   0  PASS
 ############################################################
 _sgl_fail()
 {
+  local -r FN='_sgl_fail'
   local title
 
-  case "$1" in
-    MISC)
+  case "${1}" in
+    ERR|MISC)
       title='ERROR'
       ;;
     OPT)
@@ -47,23 +48,28 @@ _sgl_fail()
       title='SUPERGLUE ERROR'
       ;;
     *)
+      if [[ ! "${1}" =~ ^[1-9][0-9]{,2}$ ]] || [[ ${1} -gt 126 ]]; then
+        _sgl_err 0 SGL "invalid \`${FN}' ERR \`${1}'"
+      fi
       title='ERROR'
       ;;
   esac
 
-  if [[ ${SGL_COLOR_ON} -eq 1 ]]; then
+  if [[ "${SGL_COLOR_ON}" == '1' ]]; then
     title="${SGL_RED}${title}${SGL_UNCOLOR}"
-  elif [[ ${SGL_COLOR_OFF} -ne 1 ]] && [[ -t 1 ]]; then
+  elif [[ "${SGL_COLOR_OFF}" != '1' ]] && [[ -t 1 ]]; then
     title="${SGL_RED}${title}${SGL_UNCOLOR}"
   fi
 
-  if [[ ${SGL_SILENT} -ne 1 ]]; then
-    printf "%s\n" "${title} $2" 1>&2
-    if [[ ${SGL_VERBOSE} -eq 1 ]]; then
+  if [[ "${SGL_SILENT}" != '1' ]]; then
+    printf '%s\n' "${title} ${2}" 1>&2
+    if [[ "${SGL_VERBOSE}" == '1' ]]; then
       local details="$(caller)"
-      printf "%s %s %s\n" '-' 'LINE' "${details%% *}" 1>&2
-      printf "%s %s %s\n" '-' 'FILE' "${details##* }" 1>&2
+      printf '%s %s %s\n' '-' 'LINE' "${details%% *}" 1>&2
+      printf '%s %s %s\n' '-' 'FILE' "${details##* }" 1>&2
     fi
   fi
+
+  return 0
 }
 readonly -f _sgl_fail
