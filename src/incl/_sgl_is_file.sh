d869d93e634a78d8ab14950a29b377eb32b12146
Adam Smith <adam@imaginate.life>
2017-02-13 02:28:37 -0800

    Added helper: _sgl_is_file

---
+++ src/incl/_sgl_is_file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --posix
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+################################################################################
+
+############################################################
+# @func _sgl_is_file
+# @use _sgl_is_file FILE
+# @val FILE  Should be a valid file path.
+# @return
+#   0  PASS
+#   1  FAIL
+############################################################
+_sgl_is_file()
+{
+  if _sgl_is_path "${1}" && [[ -f "${1}" ]]; then
+    return 0
+  fi
+  return 1
+}
+readonly -f _sgl_is_file
