f30a2818ac831827234e1c10b5221e470854bba2
Adam Smith <adam@imaginate.life>
2017-02-13 02:08:30 -0800

    Added helper: _sgl_is_dir

---
+++ src/incl/_sgl_is_dir.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --posix
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+################################################################################
+
+############################################################
+# @func _sgl_is_dir
+# @use _sgl_is_dir DIR
+# @val DIR  Should be a valid directory path.
+# @return
+#   0  PASS
+#   1  FAIL
+############################################################
+_sgl_is_dir()
+{
+  if _sgl_is_path "${1}" && [[ -d "${1}" ]]; then
+    return 0
+  fi
+  return 1
+}
+readonly -f _sgl_is_dir
