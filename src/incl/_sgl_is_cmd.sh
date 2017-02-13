8999f0e7bbc33d9cbfe563b6d94095ff188b2f0f
Adam Smith <adam@imaginate.life>
2017-02-13 01:56:16 -0800

    Added helper: _sgl_is_cmd

---
+++ src/incl/_sgl_is_cmd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash --posix
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+################################################################################
+
+############################################################
+# @func _sgl_is_cmd
+# @use _sgl_is_cmd CMD
+# @val CMD  Should be a valid path to an executable.
+# @return
+#   0  PASS
+############################################################
+_sgl_is_cmd()
+{
+  if _sgl_is_path "${1}" && [[ -x "${1}" ]]; then
+    return 0
+  fi
+  return 1
+}
+readonly -f _sgl_is_cmd
