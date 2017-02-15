4b8baf589060594404acdb8ab9315f59e6b058b2
Adam Smith <adam@imaginate.life>
2017-02-15 03:23:00 -0800

    Updated func: sgl_chk_exit

--- src/lib/sgl_chk_exit.sh
+++ src/lib/sgl_chk_exit.sh
@@ -27,13 +27,13 @@
 # @val CODE  Must be an integer in the range of `0' to `255'.
 # @val ERR   Must be an error from the below options or any valid integer in the
 #            range of `1' to `126'.
-#   `MISC'  An unknown error (exit= `1').
-#   `OPT'   An invalid option (exit= `2').
-#   `VAL'   An invalid or missing value (exit= `3').
-#   `AUTH'  A permissions error (exit= `4').
-#   `DPND'  A dependency error (exit= `5').
-#   `CHLD'  A child process exited unsuccessfully (exit= `6').
-#   `SGL'   A `superglue' script error (exit= `7').
+#   `ERR|MISC'  An unknown error (exit= `1').
+#   `OPT'       An invalid option (exit= `2').
+#   `VAL'       An invalid or missing value (exit= `3').
+#   `AUTH'      A permissions error (exit= `4').
+#   `DPND'      A dependency error (exit= `5').
+#   `CHLD'      A child process exited unsuccessfully (exit= `6').
+#   `SGL'       A `superglue' script error (exit= `7').
 # @val MSG   Can be any string. The patterns, `CMD', `PRG', and `CODE', are
 #            substituted with the proper values. The default MSG is:
 #              ``CMD'[ in `PRG'] exited with `CODE''
@@ -49,16 +49,23 @@ sgl_chk_exit()
   local -i len
   local -i code=0
   local -i xcode=0
-  local -i quiet=${SGL_QUIET}
-  local -i silent=${SGL_SILENT}
+  local -i quiet=0
+  local -i silent=0
   local cmd='a command'
   local err=CHLD
   local msg
   local prg
   local opt
 
+  if [[ "${SGL_QUIET}" == '1' ]] || [[ "${SGL_QUIET_PARENT}" == '1' ]]; then
+    quiet=1
+  fi
+  if [[ "${SGL_SILENT}" == '1' ]] || [[ "${SGL_SILENT_PARENT}" == '1' ]]; then
+    silent=1
+  fi
+
   # parse each argument
-  _sgl_parse_args "${FN}"  \
+  _sgl_parse_args ${silent} "${FN}" \
     '-h|-?|--help'       0 \
     '-c|--cmd|--command' 1 \
     '-m|--msg|--message' 1 \
@@ -67,15 +74,15 @@ sgl_chk_exit()
     '-q|--quiet'         0 \
     '-v|--version'       0 \
     '-x|--exit'          2 \
-    -- "$@"
+    -- "${@}"
 
   # parse each OPTION
   len=${#_SGL_OPTS[@]}
-  for ((i=0; i<len; i++)); do
+  for (( i=0; i<len; i++ )); do
     opt="${_SGL_OPTS[${i}]}"
     case "${opt}" in
       -h|-\?|--help)
-        _sgl_help sgl_chk_exit
+        _sgl_help ${FN}
         ;;
       -c|--cmd|--command)
         cmd="${_SGL_OPT_VALS[${i}]}"
@@ -96,37 +103,49 @@ sgl_chk_exit()
         _sgl_version
         ;;
       -x|--exit)
-        [[ ${_SGL_OPT_BOOL[${i}]} -eq 1 ]] && err="${_SGL_OPT_VALS[${i}]}"
+        if [[ ${_SGL_OPT_BOOL[${i}]} -eq 1 ]]; then
+          err="${_SGL_OPT_VALS[${i}]}"
+        fi
         xcode=$(_sgl_err_code "${err}")
-        [[ ${xcode} -eq 0 ]] && _sgl_err VAL "invalid \`${FN}' ERR \`${err}'"
+        if [[ ${xcode} -eq 0 ]]; then
+          _sgl_err ${silent} VAL "invalid \`${FN}' ERR \`${err}'"
+        fi
         ;;
       *)
-        _sgl_err SGL "invalid parsed \`${FN}' OPTION \`${opt}'"
+        _sgl_err ${silent} SGL "invalid parsed \`${FN}' OPTION \`${opt}'"
         ;;
     esac
   done
 
   # parse CODE
-  [[ ${#_SGL_VALS[@]} -gt 0 ]] || _sgl_err VAL "missing \`${FN}' CODE"
-  [[ ${#_SGL_VALS[@]} -eq 1 ]] || _sgl_err VAL "only 1 \`${FN}' CODE allowed"
+  if [[ ${#_SGL_VALS[@]} -eq 0 ]]; then
+    _sgl_err ${silent} VAL "missing \`${FN}' CODE"
+  elif [[ ${#_SGL_VALS[@]} -gt 1 ]]; then
+    _sgl_err ${silent} VAL "only 1 \`${FN}' CODE allowed"
+  fi
   code="${_SGL_VALS[0]}"
   if [[ ! "${code}" =~ ^[0-9][0-9]?[0-9]?$ ]] || [[ ${code} -gt 255 ]]; then
-    _sgl_err VAL "invalid \`${FN}' CODE \`${code}'"
+    _sgl_err ${silent} VAL "invalid \`${FN}' CODE \`${code}'"
   fi
-  [[ ${code} -eq 0 ]] && return 0
+  if [[ ${code} -eq 0 ]]; then
+    return 0
+  fi
 
   # build error message
-  if [[ ${silent} -ne 1 ]] && [[ ${SGL_SILENT_PARENT} -ne 1 ]]; then
+  if [[ ${silent} -ne 1 ]]; then
     if [[ -n "${prg}" ]]; then
       if [[ -n "${msg}" ]]; then
-        msg="$(printf '%s' "${msg}" | ${sed} -e "s|CMD|${cmd}|g" \
-          -e "s|PRG|${prg}|g" -e "s|CODE|${code}|g")"
+        prg="$(_sgl_escape_val "${prg}")"
+        cmd="$(_sgl_escape_val "${cmd}")"
+        msg="$(printf '%s' "${msg}" | ${sed} -e "s/CMD/${cmd}/g" \
+          -e "s/PRG/${prg}/g" -e "s/CODE/${code}/g")"
       else
         msg="\`${cmd}' in \`${prg}' exited with \`${code}'"
       fi
     elif [[ -n "${msg}" ]]; then
-      msg="$(printf '%s' "${msg}" | ${sed} -e "s|CMD|${cmd}|g" \
-        -e "s|CODE|${code}|g")"
+      cmd="$(_sgl_escape_val "${cmd}")"
+      msg="$(printf '%s' "${msg}" | ${sed} -e "s/CMD/${cmd}/g" \
+        -e "s/CODE/${code}/g")"
     else
       msg="\`${cmd}' exited with \`${code}'"
     fi
@@ -134,12 +153,16 @@ sgl_chk_exit()
 
   # exit process
   if [[ ${xcode} -ne 0 ]]; then
-    [[ -n "${msg}" ]] && _sgl_err ${err} "${msg}"
+    if [[ -n "${msg}" ]]; then
+      _sgl_err ${silent} ${err} "${msg}"
+    fi
     exit ${xcode}
   fi
 
   # print error
-  [[ -n "${msg}" ]] && _sgl_fail MISC "${msg}"
+  if [[ -n "${msg}" ]]; then
+    _sgl_fail ${err} "${msg}"
+  fi
 
   return 1
 }
