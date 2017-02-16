76d1f34cf15eb95d2a63204c153d840e3d90c7d2
Adam Smith <adam@imaginate.life>
2017-02-16 05:54:10 -0800

    Added helper: _sgl_match_func

---
+++ src/incl/_sgl_match_func.sh
@@ -0,0 +1,45 @@
+#!/bin/bash --posix
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+################################################################################
+
+############################################################
+# @func _sgl_match_func
+# @use _sgl_match_func FUNC
+# @val FUNC  Must be a valid `superglue' function. The `sgl_' prefix
+#            is optional. The FUNC may contain `*' for pattern matching.
+# @return
+#   0  PASS  FUNC is a valid function pattern.
+#   1  FAIL  FUNC is not a valid function pattern.
+############################################################
+_sgl_match_func()
+{
+  local func="${1}"
+  local fn
+  local re
+
+  if [[ ! "${func}" =~ \* ]]; then
+    if _sgl_is_func "${func}"; then
+      return 0
+    fi
+    return 1
+  fi
+
+  re='^[a-z_*]+$'
+  if [[ ! "${func}" =~ ${re} ]]; then
+    return 1
+  fi
+
+  func="${func#sgl_}"
+  func="sgl_${func}"
+  re="$(printf '%s' "${func}" | ${sed} -e 's/\*/[a-z_]*/g')"
+  re="^${re}\$"
+  for fn in "${SGL_FUNCS[@]}"; do
+    if [[ "${fn}" =~ ${re} ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+readonly -f _sgl_match_func
