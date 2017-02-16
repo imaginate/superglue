9340c02f934c36ea9af4c639df865a0814e6d7c3
Adam Smith <adam@imaginate.life>
2017-02-16 06:41:46 -0800

    Rebuilt helper: _sgl_help

--- src/incl/_sgl_help.sh
+++ src/incl/_sgl_help.sh
@@ -10,35 +10,31 @@
 # @val CMD   Must be a valid `superglue' command.
 # @val FUNC  Must be a valid `superglue' function.
 # @exit
-#   0  PASS
+#   0  PASS  A successful exit.
+#   6  CHLD  A child process exited unsuccessfully.
+#   7  SGL   A `superglue' script error.
 ############################################################
 _sgl_help()
 {
+  local -r FN='_sgl_help'
   local name="${1}"
-  local dir="${SGL_HELP}"
   local path
-  local -i rt
-
-  if ! _sgl_is_dir "${dir}"; then
-    _sgl_err 0 DPND "missing help dir \`${dir}' - reinstall \`${SGL}'"
-  fi
+  local -i ret
 
   if [[ "${name}" == 'sgl' ]] || [[ "${name}" == 'sglue' ]]; then
     name='superglue'
   fi
 
-  path="${dir}/${name}"
-
-  if ! _sgl_is_read "${path}"; then
-    _sgl_err 0 DPND "missing help file \`${path}' - reinstall \`${SGL}'"
+  if [[ "${name}" != 'superglue' ]] && ! _sgl_is_func "${name}"; then
+    _sgl_err 0 SGL "invalid \`${FN}' CMD|FUNC \`${name}'"
   fi
 
+  path="${SGL_HELP}/${name}"
   ${cat} -- "${path}"
-  rt=${?}
-  if [[ ${rt} -ne 0 ]]; then
-    _sgl_err 0 CHLD "\`${cat} -- \"${path}\"' exited with \`${rt}'"
+  ret=${?}
+  if [[ ${ret} -ne 0 ]]; then
+    _sgl_err 0 CHLD "\`${cat} -- \"${path}\"' exited with \`${ret}'"
   fi
-
   exit 0
 }
 readonly -f _sgl_help
