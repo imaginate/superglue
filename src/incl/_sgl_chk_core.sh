7f125e12256d3ca51cc84be11dcd3ff73ee82bb3
Adam Smith <adam@imaginate.life>
2017-02-16 06:18:12 -0800

    Added helper: _sgl_chk_core

---
+++ src/incl/_sgl_chk_core.sh
@@ -0,0 +1,34 @@
+#!/bin/bash --posix
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+################################################################################
+
+############################################################
+# @func _sgl_chk_core
+# @use _sgl_chk_core DIR ...FILE
+# @val DIR   Must be a valid `superglue' directory path.
+# @val FILE  Must be a valid filename within DIR.
+# @return
+#   0  PASS
+# @exit-on-error
+############################################################
+_sgl_chk_core()
+{
+  local dir="${1}"
+  local path
+
+  if ! _sgl_is_dir "${dir}"; then
+    _sgl_err 0 DPND "missing core directory \`${dir}' - reinstall \`${SGL}'"
+  fi
+  shift
+
+  for path in "${@}"; do
+    path="${dir}/${path}"
+    if ! _sgl_is_read "${path}"; then
+      _sgl_err 0 DPND "missing core file \`${path}' - reinstall \`${SGL}'"
+    fi
+  done
+  return 0
+}
+readonly -f _sgl_chk_core
