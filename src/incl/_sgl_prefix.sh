185fafdc18a79c6c2ee48d6e581f2b3c843e3f03
Adam Smith <adam@imaginate.life>
2017-02-16 06:39:21 -0800

    Replaced helper: _sgl_get_func -> _sgl_prefix

---
+++ src/incl/_sgl_prefix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --posix
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+################################################################################
+
+############################################################
+# @func _sgl_prefix
+# @use _sgl_prefix FUNC
+# @val FUNC  Should be a valid `superglue' function. The `sgl_' prefix
+#            is optional. The FUNC may contain `*' for pattern matching.
+# @return
+#   0  PASS
+############################################################
+_sgl_prefix()
+{
+  printf '%s%s' 'sgl_' "${1#sgl_}"
+}
+readonly -f _sgl_prefix
