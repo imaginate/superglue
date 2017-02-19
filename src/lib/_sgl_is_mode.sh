89f29ca6a4c1ade409d4b404551cfbf6efd6a775
Adam Smith <adam@imaginate.life>
2017-02-19 03:30:01 -0800

    Renamed helper: _sgl_is_mod -> _sgl_is_mode

--- src/lib/_sgl_is_mode.sh
+++ src/lib/_sgl_is_mode.sh
@@ -1,24 +1,24 @@
-# @dest $LIB/superglue/_sgl_is_mod
+# @dest $LIB/superglue/_sgl_is_mode
 # @mode 0644
 #
 # @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
 # @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
 #
-# @use _sgl_source is_mod
+# @use _sgl_source is_mode
 # @return
 #   0  PASS
 ################################################################################
 
 ############################################################
 # @private
-# @func _sgl_is_mod
-# @use _sgl_is_mod MODE
+# @func _sgl_is_mode
+# @use _sgl_is_mode MODE
 # @val MODE  Should be a valid file mode.
 # @return
 #   0  PASS  The MODE is valid.
 #   1  FAIL  The MODE is invalid.
 ############################################################
-_sgl_is_mod()
+_sgl_is_mode()
 {
   if [[ -n "${1}" ]]; then
     if [[ "${1}" =~ ^[ugoa]*([-+=]([rwxXst]+|[ugo]))+$ ]] || \
@@ -29,4 +29,4 @@ _sgl_is_mod()
   fi
   return 1
 }
-readonly -f _sgl_is_mod
+readonly -f _sgl_is_mode
