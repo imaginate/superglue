4261e07b79ec371af0c77dd471a5e1e38bcd923b
Adam Smith <adam@imaginate.life>
2017-02-19 07:11:52 -0800

    Added helper: _sgl_has_key

---
+++ src/lib/_sgl_has_key.sh
@@ -0,0 +1,28 @@
+# @dest $LIB/superglue/_sgl_has_key
+# @mode 0644
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+#
+# @use _sgl_source has_key
+# @return
+#   0  PASS
+################################################################################
+
+############################################################
+# @private
+# @func _sgl_has_key
+# @use _sgl_has_key VALUE
+# @val VALUE  Must be a TAG value.
+# @return
+#   0  PASS  The VALUE contains at least one KEY.
+#   1  FAIL  The VALUE does not contain a KEY.
+############################################################
+_sgl_has_key()
+{
+  if [[ "${1}" =~ ^\$ ]] || [[ "${1}" =~ [^\\]\$ ]]; then
+    return 0
+  fi
+  return 1
+}
+readonly -f _sgl_has_key
