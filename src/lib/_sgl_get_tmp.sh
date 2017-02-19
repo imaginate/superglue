a4c61b4f027f0ed91cb4bfd06c1cb1030dc3a8ff
Adam Smith <adam@imaginate.life>
2017-02-19 05:12:41 -0800

    Added helper: _sgl_get_tmp

---
+++ src/lib/_sgl_get_tmp.sh
@@ -0,0 +1,45 @@
+# @dest $LIB/superglue/_sgl_get_tmp
+# @mode 0644
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+#
+# @use _sgl_source get_tmp
+# @return
+#   0  PASS
+################################################################################
+
+############################################################
+# @private
+# @func _sgl_get_tmp
+# @use _sgl_get_tmp [PREFIX]
+# @val PREFIX  Can be any string.
+# @return
+#   0  PASS
+############################################################
+_sgl_get_tmp()
+{
+  local base="${TMP}/.sgl"
+  local pre="${1}"
+  local -ir MAX=9999
+  local -i i=0
+
+  if [[ -n "${pre}" ]]; then
+    pre=".${pre##.}"
+    pre="${pre%%.}"
+  fi
+
+  while _sgl_is_file "${base}${pre}.${i}";
+    i=$(( i + 1 ))
+    if [[ ${i} -gt ${MAX} ]]; then
+      if [[ "${pre}" =~ \.x+$ ]]; then
+        _sgl_get_tmp "${pre}x"
+      else
+        _sgl_get_tmp "${pre}.x"
+      fi
+      return 0
+    fi
+  done
+  printf '%s' "${base}${pre}.${i}"
+}
+readonly -f _sgl_get_tmp
