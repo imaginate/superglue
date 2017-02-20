aeb00b8cb1665f7ac46b603729735e1b2187223b
Adam Smith <adam@imaginate.life>
2017-02-20 14:14:22 -0800

    Added checks to _sgl_chk_tag

--- src/lib/_sgl_chk_tag.sh
+++ src/lib/_sgl_chk_tag.sh
@@ -9,7 +9,7 @@
 #   0  PASS
 ################################################################################
 
-_sgl_source err get_keys has_def has_tag is_key trim_tag
+_sgl_source cnt_tag err get_keys has_def has_tag is_key trim_blank trim_tag
 
 ############################################################
 # @private
@@ -37,41 +37,89 @@ _sgl_source err get_keys has_def has_tag is_key trim_tag
 ############################################################
 _sgl_chk_tag()
 {
-  local prg="${1}"
-  local src="${2}"
-  local tag="${3}"
+  local -r PRG="${1}"
+  local -r SRC="${2}"
+  local -r TAG="${3}"
   local key
   local val
   local line
   local name
+  local patt
+  local -i var=0
+  local -i limit=0
 
-  if ! _sgl_has_tag "${src}" "${tag}"; then
+  if ! _sgl_has_tag "${SRC}" "${TAG}"; then
     return 0
   fi
 
-  case "${tag}" in
+  case "${TAG}" in
     DEST)
-      tag='^[[:blank:]]*#[[:blank:]]*@dest\(ination\)\?[[:blank:]]\+'
+      patt='^[[:blank:]]*#[[:blank:]]*@dest\(ination\)\?[[:blank:]]\+'
       ;;
     INCL)
-      tag='^[[:blank:]]*#[[:blank:]]*@incl\(ude\)\?[[:blank:]]\+'
+      patt='^[[:blank:]]*#[[:blank:]]*@incl\(ude\)\?[[:blank:]]\+'
       ;;
     MODE)
-      tag='^[[:blank:]]*#[[:blank:]]*@mode\?[[:blank:]]\+'
+      patt='^[[:blank:]]*#[[:blank:]]*@mode\?[[:blank:]]\+'
+      name='mode'
+      limit=1
       ;;
     OWN)
-      tag='^[[:blank:]]*#[[:blank:]]*@own\(er\)\?[[:blank:]]\+'
+      patt='^[[:blank:]]*#[[:blank:]]*@own\(er\)\?[[:blank:]]\+'
+      name='owner'
+      limit=1
       ;;
     SET)
-      tag='^[[:blank:]]*#[[:blank:]]*@\(set\|var\|variable\)[[:blank:]]\+'
+      patt='^[[:blank:]]*#[[:blank:]]*@\(set\|var\|variable\)[[:blank:]]\+'
+      var=1
       ;;
     VERS)
-      tag='^[[:blank:]]*#[[:blank:]]*@vers\(ion\)\?[[:blank:]]\+'
+      patt='^[[:blank:]]*#[[:blank:]]*@vers\(ion\)\?[[:blank:]]\+'
+      name='version'
+      limit=1
       ;;
   esac
 
+  if [[ ${limit} -gt 0 ]]; then
+    if [[ $(_sgl_cnt_tag "${SRC}" ${TAG}) -gt ${limit} ]]; then
+      name="${limit} \`${name}' TAG"
+      _sgl_err VAL "only ${name} allowed for \`${PRG}' SRC \`${SRC}'"
+    fi
+  fi
+
   while IFS= read -r line; do
     val="$(_sgl_trim_tag "${line}")"
+
+    if [[ ${var} -eq 1 ]]; then
+      key="$(_sgl_trim_blank "${val%%=*}")"
+      if ! _sgl_is_key "${key}"; then
+        key="KEY \`${key}' at LINE \`${line}'"
+        _sgl_err VAL "invalid \`${PRG}' ${key} in SRC \`${SRC}'"
+      fi
+      if [[ ! "${val}" =~ = ]]; then
+        val="VAL at LINE \`${line}'"
+        _sgl_err VAL "missing \`${PRG}' ${val} in SRC \`${SRC}'"
+      fi
+      val="$(_sgl_trim_blank "${val#*=}")"
+    fi
+
+    if [[ "${val:0:1}" == "'" ]]; then
+      if [[ "${val: -1}" != "'" ]]; then
+        val="VALUE \`${val}' at LINE \`${line}'"
+        _sgl_err VAL "invalid \`${PRG}' ${val} in SRC \`${SRC}'"
+      fi
+      continue
+    fi
+
+    if [[ "${val:0:1}" == '"' ]]; then
+      if [[ "${val: -1}" != '"' ]]; then
+        val="VALUE \`${val}' at LINE \`${line}'"
+        _sgl_err VAL "invalid \`${PRG}' ${val} in SRC \`${SRC}'"
+      fi
+      val="${val:1}"
+      val="${val%\"}"
+    fi
+
     while IFS= read -r key; do
       if [[ -z "${key}" ]]; then
         continue
@@ -81,7 +129,7 @@ _sgl_chk_tag()
       if [[ "${key:1:1}" == '{' ]]; then
         if [[ "${key: -1:1}" != '}' ]]; then
           key="KEY \`${key}' at LINE \`${line}'"
-          _sgl_err VAL "invalid \`${prg}' ${key} in SRC \`${src}'"
+          _sgl_err VAL "invalid \`${PRG}' ${key} in SRC \`${SRC}'"
         fi
         name="${name:1}"
         name="${name%\}}"
@@ -89,15 +137,15 @@ _sgl_chk_tag()
 
       if ! _sgl_is_key "${name}"; then
         key="KEY \`${key}' at LINE \`${line}'"
-        _sgl_err VAL "invalid \`${prg}' ${key} in SRC \`${src}'"
+        _sgl_err VAL "invalid \`${PRG}' ${key} in SRC \`${SRC}'"
       fi
 
       if ! _sgl_has_def "${name}"; then
         key="KEY \`${key}' at LINE \`${line}'"
-        _sgl_err VAL "undefined \`${prg}' ${key} in SRC \`${src}'"
+        _sgl_err VAL "undefined \`${PRG}' ${key} in SRC \`${SRC}'"
       fi
     done <<< "$(_sgl_get_keys "${val}")"
-  done <<< "$(${grep} -e "${tag}" -- "${src}")"
+  done <<< "$(${grep} -e "${patt}" -- "${SRC}")"
 
   return 0
 }
