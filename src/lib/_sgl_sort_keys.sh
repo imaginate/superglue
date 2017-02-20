f9e9c4874e6253b758358fef0f0a6a648152e0f7
Adam Smith <adam@imaginate.life>
2017-02-20 15:54:15 -0800

    Added helper: _sgl_sort_keys

---
+++ src/lib/_sgl_sort_keys.sh
@@ -0,0 +1,37 @@
+# @dest $LIB/superglue/_sgl_sort_keys
+# @mode 0644
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+#
+# @use _sgl_source sort_keys
+# @return
+#   0  PASS
+################################################################################
+
+############################################################
+# @private
+# @func _sgl_sort_keys
+# @use _sgl_sort_keys [...KEY]
+# @val KEY  Must be a valid VAR key.
+# @return
+#   0  PASS
+############################################################
+_sgl_sort_keys()
+{
+  case ${#} in
+    0)
+      ;;
+    1)
+      printf '%s\n' "${1}"
+      ;;
+    *)
+      local key
+      for key in "${@}"; do
+        printf '%s\n' "${key}"
+      done | ${sort} -u
+      ;;
+  esac
+  return 0
+}
+readonly -f _sgl_sort_keys
