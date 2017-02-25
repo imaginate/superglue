66d8541101f3a5d289ce8a3dabc03a8de96a7b91
Adam Smith <adam@imaginate.life>
2017-02-24 17:32:57 -0800

    Added helper: _sgl_get_mode

---
+++ src/lib/_sgl_get_mode.sh
@@ -0,0 +1,57 @@
+# @dest $LIB/superglue/_sgl_get_mode
+# @mode 0644
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+#
+# @use _sgl_source get_mode
+# @return
+#   0  PASS
+################################################################################
+
+_sgl_source is_path
+
+############################################################
+# @private
+# @func _sgl_get_mode
+# @use _sgl_get_mode PATH
+# @val PATH  Must be a valid file system path.
+# @return
+#   0  PASS
+############################################################
+_sgl_get_mode()
+{
+  local -r PATH="${1}"
+  local -i mod
+  local mode
+  local val
+
+  if ! _sgl_is_path "${PATH}"; then
+    return 0
+  fi
+
+  val="$(${ls} -b -d -l -L -- "${PATH}")"
+  val="${val%% *}"
+
+  if [[ -z "${val}" ]] || [[ ! "${val}" =~ ^[-d]([-r][-w][-x]){3}$ ]]; then
+    return 0
+  fi
+
+  mode='0'
+  for val in "${val:1:3}" "${val:4:3}" "${val:7:3}"; do
+    mod=0
+    if [[ "${val:0:1}" == 'r' ]]; then
+      mod=$(( mod + 4 ))
+    fi
+    if [[ "${val:1:1}" == 'w' ]]; then
+      mod=$(( mod + 2 ))
+    fi
+    if [[ "${val:2:1}" == 'x' ]]; then
+      mod=$(( mod + 1 ))
+    fi
+    mode="${mode}${mod}"
+  done
+
+  printf '%s' "${mode}"
+}
+readonly -f _sgl_get_mode
