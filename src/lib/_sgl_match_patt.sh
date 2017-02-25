08c41878c37be35ed44d4a0e6c00dc7f4ae569d6
Adam Smith <adam@imaginate.life>
2017-02-24 18:24:31 -0800

    Added helper: _sgl_match_patt

---
+++ src/lib/_sgl_match_patt.sh
@@ -0,0 +1,36 @@
+# @dest $LIB/superglue/_sgl_match_patt
+# @mode 0644
+#
+# @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
+# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
+#
+# @use _sgl_source match_patt
+# @return
+#   0  PASS
+################################################################################
+
+############################################################
+# @private
+# @func _sgl_match_patt
+# @use _sgl_match_patt PATT STR
+# @val PATT  Must be a valid basic regular expression.
+# @val STR   Can be any string.
+# @return
+#   0  PASS  The STR matches the PATT.
+#   1  FAIL  The STR does not match the PATT.
+############################################################
+_sgl_match_patt()
+{
+  local -r PATT="${1}"
+  local -r STR="${2}"
+
+  if [[ -z "${PATT}" ]]; then
+    return 1
+  fi
+
+  if ${grep} -q -e "${PATT}" <<< "${STR}"; then
+    return 0
+  fi
+  return 1
+}
+readonly -f _sgl_match_patt
