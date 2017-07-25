5cea5df2587c977bd677396478be36a5781b3ee7
Adam Smith <adam@imaginate.life>
2017-07-25 04:20:27 -0700

    Rebuilt test init script

--- test/bin/test.sh
+++ test/bin/test.sh
@@ -5,13 +5,14 @@
 # @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
 # @copyright 2016-2017 Adam A Smith <adam@imaginate.life>
 #
-# @use ./run.tests [...SECTION]
+# @use ./test.sh [...SECTION]
 # @val SECTION  If a SECTION is defined, it must be a readable file path to
-#               `../test/SECTION.test'. If no SECTION is defined, all tests
-#               (i.e. all file paths matching `../test/*.test') are ran.
+#               `../tests/SECTION.sh'. If no SECTION is defined, all tests
+#               (i.e. all file paths matching `../tests/*.sh') are ran.
 # @exit
-#   0  PASS  All tests passed.
-#   1  FAIL  One or more tests failed.
+#   0  PASS   All tests passed.
+#   1  FAIL   One or more tests failed.
+#   9  ERROR  An internal error occurred (i.e. **no** tests were ran).
 ##############################################################################
 
 ##############################################################################
@@ -22,52 +23,52 @@ if [[ ! "${0}" =~ ^(\./)?run\.tests$ ]]; then
   cd "${0%/*}"
 fi
 
-##############################################################################
-## LOAD REFS & HELPERS
-##############################################################################
-
-source ./test.refs
-source ./echo.helpers
-source ./test.helpers
+cd ..
 
 ##############################################################################
-## MAKE DUMMY DIRECTORIES
+## SETUP HELPERS
 ##############################################################################
 
-for DIR in "${DUMMY_DIRS[@]}"; do
-  if [[ ! -d "${DIR}" ]]; then
-    mkdir -p -m 0755 -- "${DIR}"
-  fi
-done
+. ./helpers/refs.sh
+. ./helpers/general.sh
+. ./helpers/print.sh
+. ./helpers/test.sh
+
+##############################################################################
+## SETUP DUMMY DIRECTORIES
+##############################################################################
+
+sglue_mk_dir -m 0755 -p -- "${SGLUE_DUMMY_DIRS[@]}"
 
 ##############################################################################
 ## RUN TESTS
 ##############################################################################
 
-header
+sglue_header
 
 if [[ ${#} -gt 0 ]]; then
-  run_tests "${@}"
+  sglue_run_tests "${@}"
 else
-  run_tests ./*.test
+  sglue_run_tests
 fi
 
-results
-footer
+sglue_results
+sglue_footer
 
 ##############################################################################
 ## REMOVE DUMMY DIRECTORIES
 ##############################################################################
 
-if [[ -d "${DUMMY}" ]]; then
-  rm -r -f -- "${DUMMY}"
+if [[ -d "${SGLUE_DUMMY}" ]]; then
+  sglue_clean_tree "${SGLUE_DUMMY}"
+  sglue_rm_dir -r -- "${SGLUE_DUMMY}"
 fi
 
 ##############################################################################
 ## EXIT WITH RESULT
 ##############################################################################
 
-if [[ ${TESTS_FAILED} -gt 0 ]]; then
+if [[ ${SGLUE_TESTS_FAILED} -gt 0 ]]; then
   exit 1
 fi
 exit 0
