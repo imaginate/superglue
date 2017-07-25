53f881271e8bd2f944ad6b91bbd2fb86c1ce9097
Adam Smith <adam@imaginate.life>
2017-07-25 06:30:43 -0700

    Added test helper ref for chmod

--- test/helpers/refs.sh
+++ test/helpers/refs.sh
@@ -244,9 +244,10 @@ _sglue_get_cmd()
   return 0
 }
 
-_sglue_chk_cmds cat grep mkdir ls rm sed
+_sglue_chk_cmds cat chmod grep mkdir ls rm sed
 
 declare -r -x SGLUE_CAT="$(_sglue_get_cmd cat)"
+declare -r -x SGLUE_CHMOD="$(_sglue_get_cmd chmod)"
 declare -r -x SGLUE_GREP="$(_sglue_get_cmd grep)"
 declare -r -x SGLUE_MKDIR="$(_sglue_get_cmd mkdir)"
 declare -r -x SGLUE_LS="$(_sglue_get_cmd ls)"
