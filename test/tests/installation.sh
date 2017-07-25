d637866364d3db2572da72fe4df1606eca8b3e87
Adam Smith <adam@imaginate.life>
2017-07-25 06:08:10 -0700

    Rebuilt install tests

--- test/tests/installation.sh
+++ test/tests/installation.sh
@@ -1,45 +1,57 @@
-#!/bin/bash
-#
-# Verify the installation.
+# Superglue Installation Tests
+# ============================
 #
 # @author Adam Smith <adam@imaginate.life> (http://imaginate.life)
-# @copyright 2017 Adam A Smith <adam@imaginate.life> (http://imaginate.life)
-################################################################################
+# @copyright 2016-2017 Adam A Smith <adam@imaginate.life>
+##############################################################################
 
 local name
 local path
 
-################################################################################
+##############################################################################
 ## CHECK BIN
-################################################################################
+##############################################################################
 
-for name in "${SGLUE_CMDS[@]}"; do
-  path="${BIN}/${name}"
-  if [[ ! -x "${path}" ]]; then
-    throw "missing executable path \`${path}'"
-  fi
-done
+if sglue_is_dir -r -- "${SGLUE_BIN}"; then
+  for name in "${SGLUE_CMDS[@]}"; do
+    path="${SGLUE_BIN}/${name}"
+    if ! sglue_is_file -r -x -- "${path}"; then
+      sglue_throw "missing executable file \`${path}'"
+    fi
+  done
+else
+  sglue_throw "missing readable dir \`${SGLUE_BIN}'"
+fi
 
-################################################################################
+##############################################################################
 ## CHECK LIB
-################################################################################
+##############################################################################
 
-for name in "${SGLUE_FUNCS[@]}"; do
-  path="${LIB}/${name}"
-  if [[ ! -r "${path}" ]]; then
-    throw "missing readable path \`${path}'"
-  fi
-done
+if sglue_is_dir -r -- "${SGLUE_LIB}"; then
+  for name in "${SGLUE_FUNCS[@]}"; do
+    path="${SGLUE_LIB}/${name}"
+    if ! sglue_is_file -r -- "${path}"; then
+      sglue_throw "missing readable file \`${path}'"
+    fi
+  done
+else
+  sglue_throw "missing readable dir \`${SGLUE_LIB}'"
+fi
 
-################################################################################
+##############################################################################
 ## CHECK HELP
-################################################################################
+##############################################################################
 
-for name in superglue "${SGLUE_FUNCS[@]}"; do
-  path="${HELP}/${name}"
-  if [[ ! -r "${path}" ]]; then
-    throw "missing readable path \`${path}'"
-  fi
-done
+if sglue_is_dir -r -- "${SGLUE_HELP}"; then
+  for name in superglue "${SGLUE_FUNCS[@]}"; do
+    path="${SGLUE_HELP}/${name}"
+    if ! sglue_is_file -r -- "${path}"; then
+      sglue_throw "missing readable file \`${path}'"
+    fi
+  done
+else
+  sglue_throw "missing readable dir \`${SGLUE_HELP}'"
+fi
+
 return 0
 
